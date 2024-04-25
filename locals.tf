751a642b865c8ccdee2bc261fdb1db7aefae89cf
Craig Sloggett <craigsloggett@gmail.com>
2024-04-25 16:15:21 -0400

    Add variables

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
   account_id  = data.aws_caller_identity.current.account_id
   region      = data.aws_region.current.name
-  bucket_name = "${local.account_id}-${local.region}-terraform-state"
-  table_name  = "${local.account_id}-${local.region}-terraform-state-locks"
+  bucket_name = "${local.account_id}-${local.region}-${var.bucket_name}"
+  table_name  = "${local.account_id}-${local.region}-${var.table_name}"
 }

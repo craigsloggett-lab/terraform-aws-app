fa2717a06837b63537f164ecb682b93a8e44503a
Craig Sloggett <craigsloggett@gmail.com>
2024-09-17 10:18:12 -0400

    chore: Testing workload identity

---
+++ main.tf
@@ -0,0 +1,3 @@
+data "environment_variable" "tfc_workload_identity_token" {
+  name = "TFC_WORKLOAD_IDENTITY_TOKEN"
+}
--- versions.tf
+++ versions.tf
@@ -6,5 +6,9 @@ terraform {
       source  = "hashicorp/aws"
       version = "5.60.0"
     }
+    environment = {
+      source  = "registry.terraform.io/morganpeat/environment"
+      version = "0.2.7"
+    }
   }
 }
